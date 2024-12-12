917f9b77169ae93560340bcbee015083e02cf94e
katia-otot <katiagadea@gmail.com>
2024-12-12 20:50:19 -0300

    mmm

---
+++ src/app/projects/project.interface.ts
@@ -0,0 +1,8 @@
+export interface Project {
+    title: string;
+    description: string;
+    imageUrl: string;
+    technologies: string[];
+    githubLink?: string;
+    demoLink?: string;
+  }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { Project } from './project.interface';
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -10,5 +10,14 @@ import { RouterModule } from '@angular/router';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
- 
+  projects: Project[] = [
+    {
+      title: 'Proyecto de MAPA LOCAL POR LA MEMORIA',
+      description: 'Aplicación web para mostrar recorridos que se pueden hacer para visitar los sitios de memoria locales',
+      imageUrl: 'https://via.placeholder.com/150',
+      technologies: ['JavaScript', 'CSS', 'HTML'],
+      githubLink: 'https://github.com/katia-otot/mapa',
+      demoLink: 'https://mapamemoria.netlify.app/'
+    },
+  ];
 }
