236381d356b4f4f66d845a79a04c2e6253a40917
katia-otot <katiagadea@gmail.com>
2025-02-25 16:19:23 -0300

    estilo mejorados

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
