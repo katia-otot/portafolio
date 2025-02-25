c2e98421681493f6b422624786dc0a2b52d9a4bf
katia-otot <katiagadea@gmail.com>
2025-02-25 17:14:23 -0300

    scroll..

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,51 +28,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     // Inicialización básica
-    this.isMobile = window.innerWidth < 768; // Inicializar isMobile
+
   }
 
   ngAfterViewInit() {
     this.sections = Array.from(document.querySelectorAll('.section'));
-    this.setupScrollListener();
+
   }
 
-  private setupScrollListener() {
-    if (this.isMobile) {
-      // Manejar el desplazamiento en dispositivos móviles
-      let startY: number;
-
-      window.addEventListener('touchstart', (event) => {
-        startY = event.touches[0].clientY; // Guardar la posición inicial del toque
-      });
-
-      window.addEventListener('touchmove', (event) => {
-        const currentY = event.touches[0].clientY; // Obtener la posición actual del toque
-        const direction = startY - currentY; // Calcular la dirección del desplazamiento
-
-        if (Math.abs(direction) > 30) { // Umbral para evitar desplazamientos accidentales
-          event.preventDefault(); // Prevenir el comportamiento predeterminado
-
-          if (direction > 0) {
-            // Desplazamiento hacia abajo
-            this.scrollToSection(this.currentSectionIndex + 1);
-          } else {
-            // Desplazamiento hacia arriba
-            this.scrollToSection(this.currentSectionIndex - 1);
-          }
-        }
-      });
-    } else {
-      // Manejar el desplazamiento en dispositivos de escritorio
-      window.addEventListener('wheel', (event) => {
-        event.preventDefault();
-        
-        if (this.isScrolling) return;
-        
-        const direction = event.deltaY > 0 ? 1 : -1;
-        this.scrollToSection(this.currentSectionIndex + direction);
-      }, { passive: false });
-    }
-  }
 
   private scrollToSection(index: number) {
     if (index < 0 || index >= this.sections.length || this.isScrolling) return;
